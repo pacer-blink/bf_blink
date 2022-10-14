178b9df387d0d10b355f454df2de9e5d8a022e2d
Danny Price <dan@thetelegraphic.com>
2022-10-14 16:28:32 +0800

    Updated setup_env.sh (mwa-blc00)

--- setup_env.sh
+++ setup_env.sh
@@ -1,3 +1,5 @@
+export CC=gcc-9
+export CXX=g++-9
 export CPATH=/home/dancpr/software/local/include:$CPATH
 export MSFITSLIB_DIR=/home/dancpr/software/bf_plugins/bf_blink/subprojects/msfitslib/build
 
